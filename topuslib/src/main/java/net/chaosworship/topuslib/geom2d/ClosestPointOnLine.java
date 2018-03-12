3ae5cc3650c43671b9b50389dcbdf2547792347b
anescient <anescient@gmail.com>
2018-03-11 20:35:04 -0500

    line segment methods from Twist Flux

---
+++ topuslib/src/main/java/net/chaosworship/topuslib/geom2d/ClosestPointOnLine.java
@@ -0,0 +1,44 @@
+package net.chaosworship.topuslib.geom2d;
+
+
+// relate a point to a line
+@SuppressWarnings("SameParameterValue")
+public class ClosestPointOnLine {
+
+    private float mAlongAB; // 0 to 1 along segment AB
+    private final Vec2 mOnAB; // point closest to Q
+
+    // find point along line AB closest to Q
+    // if segment, limit to the segment AB
+    public ClosestPointOnLine(Vec2 Q, Vec2 A, Vec2 B, boolean segment) {
+
+        float Ax2 = A.x * A.x;
+        float Ay2 = A.y * A.y;
+        float Bx2 = B.x * B.x;
+        float By2 = B.y * B.y;
+
+        // Jan.'18 from https://math.stackexchange.com/questions/1300484/distance-between-line-and-a-point?noredirect=1&lq=1
+        float denom = Ax2 - 2 * A.x * B.x + Ay2 - 2 * A.y * B.y + Bx2 + By2;
+        mAlongAB = 0;
+        if(denom == 0) {
+            if(!A.equals(B)) {
+                throw new AssertionError();
+            }
+        } else {
+            mAlongAB = (Ax2 - A.x * (B.x + Q.x) + Ay2 - A.y * (B.y + Q.y) + B.x * Q.x + B.y * Q.y) / denom;
+        }
+        if(segment) {
+            mAlongAB = mAlongAB < 0 ? 0 : mAlongAB;
+            mAlongAB = mAlongAB > 1 ? 1 : mAlongAB;
+        }
+        mOnAB = B.difference(A).scale(mAlongAB).add(A);
+    }
+
+    public Vec2 getClosestOnAB() {
+        return mOnAB;
+    }
+
+    public float getAlongAB() {
+        return mAlongAB;
+    }
+}
---
+++ topuslib/src/main/java/net/chaosworship/topuslib/geom2d/SegmentIntersection.java
@@ -0,0 +1,81 @@
+package net.chaosworship.topuslib.geom2d;
+
+import static java.lang.Float.NaN;
+
+
+// quick and dirty segment intersection
+// many special cases are not handled
+// do not use to control nuclear reactors
+@SuppressWarnings("unused")
+public class SegmentIntersection {
+
+    private boolean mSegmentsIntersect;
+    private float mAlongAB;
+    private float mAlongCD;
+    private Vec2 mIntersection;
+
+    // test/calculate intersection of segment AB with segment CD
+    public SegmentIntersection(Vec2 A, Vec2 B, Vec2 C, Vec2 D) {
+        mAlongAB = NaN;
+        mAlongCD = NaN;
+        mIntersection = null;
+
+        if(A.equals(B) || C.equals(D)) {
+            mSegmentsIntersect = false;
+        } else {
+            // https://www.cdn.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+            boolean cwABC = clockwise(A, B, C);
+            boolean cwABD = clockwise(A, B, D);
+            boolean cwCDA = clockwise(C, D, A);
+            boolean cwCDB = clockwise(C, D, B);
+            mSegmentsIntersect = cwABC != cwABD && cwCDA != cwCDB;
+        }
+
+        if(mSegmentsIntersect) {
+            // http://www.ahinson.com/algorithms_general/Sections/Geometry/ParametricLineIntersection.pdf
+            float xDC = D.x - C.x;
+            float xBA = B.x - A.x;
+            float xCA = C.x - A.x;
+            float yCA = C.y - A.y;
+            float yBA = B.y - A.y;
+            float yDC = D.y - C.y;
+            float denom = xDC * yBA - xBA * yDC;
+            if(denom == 0) {
+                mSegmentsIntersect = false;
+            } else {
+                mAlongAB = (xDC * yCA - xCA * yDC) / denom;
+                mAlongCD = (xBA * yCA - xCA * yBA) / denom;
+                mIntersection = Vec2.mix(A, B, mAlongAB);
+            }
+        }
+    }
+
+    private static boolean clockwise(Vec2 P, Vec2 Q, Vec2 R) {
+        return (Q.y - P.y) * (R.x - Q.x) - (Q.x - P.x) * (R.y - Q.y) > 0;
+    }
+
+    public boolean segmentsIntersect() {
+        return mSegmentsIntersect;
+    }
+
+    public Vec2 getIntersection() {
+        if(!mSegmentsIntersect) {
+            throw new IllegalStateException();
+        }
+        return mIntersection;
+    }
+
+    public float getAlongAB() {
+        if(!mSegmentsIntersect) {
+            throw new IllegalStateException();
+        }
+        return mAlongAB;
+    }
+
+    public float getAlongCD() {
+        if(!mSegmentsIntersect) {
+            throw new IllegalStateException();
+        }
+        return mAlongCD;
+    }
+}
