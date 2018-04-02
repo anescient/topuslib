9ff3ef76c5e1aedef15c4a3d4922f5807de1259a
anescient <anescient@gmail.com>
2018-04-01 21:40:16 -0500

    static Triangle methods

--- topuslib/src/main/java/net/chaosworship/topuslib/geom2d/Triangle.java
+++ topuslib/src/main/java/net/chaosworship/topuslib/geom2d/Triangle.java
@@ -28,28 +28,26 @@ public class Triangle {
         pointC.set(c);
     }
 
-    public boolean contains(Vec2 point) {
-        float APx = point.x - pointA.x;
-        float APy = point.y - pointA.y;
-        float ABx = pointB.x - pointA.x;
-        float ABy = pointB.y - pointA.y;
-        float ACx = pointC.x - pointA.x;
-        float ACy = pointC.y - pointA.y;
-        float BCx = pointC.x - pointB.x;
-        float BCy = pointC.y - pointB.y;
-        float BPx = point.x - pointB.x;
-        float BPy = point.y - pointB.y;
-        boolean PAB = ABx * APy - ABy * APx > 0;
-        return (ACx * APy - ACy * APx > 0 != PAB) && (BCx * BPy - BCy * BPx > 0 == PAB);
+    public ClosestPointOnLine closestPointOnBound(Vec2 point) {
+        return closestPointOnBound(pointA, pointB, pointC, point);
     }
 
-    public ClosestPointOnLine closestPointOnBound(Vec2 point) {
-        ClosestPointOnLine closestAB = new ClosestPointOnLine(point, pointA, pointB, true);
-        ClosestPointOnLine closestBC = new ClosestPointOnLine(point, pointB, pointC, true);
-        ClosestPointOnLine closestCA = new ClosestPointOnLine(point, pointC, pointA, true);
-        float dAB = Vec2.distanceSq(point, closestAB.getClosestOnAB());
-        float dBC = Vec2.distanceSq(point, closestBC.getClosestOnAB());
-        float dCA = Vec2.distanceSq(point, closestCA.getClosestOnAB());
+    public boolean contains(Vec2 point) {
+        return contains(pointA, pointB, pointC, point);
+    }
+
+    public float area() {
+        return area(pointA, pointB, pointC);
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public static ClosestPointOnLine closestPointOnBound(Vec2 a, Vec2 b, Vec2 c, Vec2 p) {
+        ClosestPointOnLine closestAB = new ClosestPointOnLine(p, a, b, true);
+        ClosestPointOnLine closestBC = new ClosestPointOnLine(p, b, c, true);
+        ClosestPointOnLine closestCA = new ClosestPointOnLine(p, c, a, true);
+        float dAB = Vec2.distanceSq(p, closestAB.getClosestOnAB());
+        float dBC = Vec2.distanceSq(p, closestBC.getClosestOnAB());
+        float dCA = Vec2.distanceSq(p, closestCA.getClosestOnAB());
         ClosestPointOnLine closest;
         if(dAB < dBC) {
             closest = dAB < dCA ? closestAB : closestCA;
@@ -59,10 +57,23 @@ public class Triangle {
         return closest;
     }
 
-    public float area() {
-        return Math.abs(
-                pointA.y * (pointC.x - pointB.x)
-                + pointB.y * (pointA.x - pointC.x)
-                + pointC.y * (pointB.x - pointA.x)) * 0.5f;
+    public static boolean contains(Vec2 a, Vec2 b, Vec2 c, Vec2 p) {
+        float APx = p.x - a.x;
+        float APy = p.y - a.y;
+        float ABx = b.x - a.x;
+        float ABy = b.y - a.y;
+        float ACx = c.x - a.x;
+        float ACy = c.y - a.y;
+        float BCx = c.x - b.x;
+        float BCy = c.y - b.y;
+        float BPx = p.x - b.x;
+        float BPy = p.y - b.y;
+        boolean PAB = ABx * APy - ABy * APx > 0;
+        return (ACx * APy - ACy * APx > 0 != PAB) && (BCx * BPy - BCy * BPx > 0 == PAB);
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public static float area(Vec2 a, Vec2 b, Vec2 c) {
+        return Math.abs(a.y * (c.x - b.x) + b.y * (a.x - c.x) + c.y * (b.x - a.x)) * 0.5f;
     }
 }
