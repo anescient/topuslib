51bfc0d374c86f7e7774a832ae45455dae4068c3
anescient <anescient@gmail.com>
2018-04-03 15:41:48 -0500

    IntPair

---
+++ topuslib/src/main/java/net/chaosworship/topuslib/IntPair.java
@@ -0,0 +1,45 @@
+package net.chaosworship.topuslib;
+
+
+public class IntPair implements Cloneable {
+
+    final int a;
+    final int b;
+    private final int hashcode;
+
+    public IntPair(int a, int b) {
+        this.a = a;
+        this.b = b;
+        hashcode = this.a ^ (this.b * 997);
+    }
+
+    public static IntPair sorted(int a, int b) {
+        if(a < b) {
+            return new IntPair(a, b);
+        } else {
+            return new IntPair(b, a);
+        }
+    }
+
+    @Override
+    protected Object clone()
+            throws CloneNotSupportedException {
+        super.clone();
+        return new IntPair(a, b);
+    }
+
+    @Override
+    public boolean equals(Object rhs) {
+        if(rhs == null || !this.getClass().equals(rhs.getClass())) {
+            return false;
+        }
+
+        IntPair rhsPair = (IntPair)rhs;
+        return a == rhsPair.a && b == rhsPair.b;
+    }
+
+    @Override
+    public int hashCode() {
+        return hashcode;
+    }
+}
---
+++ topuslib/src/test/java/net/chaosworship/topuslib/IntPairTest.java
@@ -0,0 +1,30 @@
+package net.chaosworship.topuslib;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+
+public class IntPairTest {
+
+    @Test
+    public void equalsAndHash() {
+        IntPair p1 = new IntPair(1, 2);
+        IntPair p2 = new IntPair(1, 2);
+        IntPair p3 = new IntPair(2, 1);
+        assertTrue(p1.equals(p2));
+        assertTrue(p1.hashCode() == p2.hashCode());
+        assertFalse(p1.equals(p3));
+    }
+
+    @Test
+    public void sortedIsSorted() {
+        assertFalse(isSorted(new IntPair(2, 1)));
+        assertTrue(isSorted(IntPair.sorted(1, 2)));
+        assertTrue(isSorted(IntPair.sorted(2, 1)));
+    }
+
+    private static boolean isSorted(IntPair intPair) {
+        return intPair.a <= intPair.b;
+    }
+}
