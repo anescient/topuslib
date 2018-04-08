db4c49416b44fa1e83daf4dc0f037fe12cca6e03
anescient <anescient@gmail.com>
2018-04-08 00:38:02 -0500

    Delaunay edge enumeration test

--- topuslib/src/test/java/net/chaosworship/topuslib/DelaunayTest.java
+++ topuslib/src/test/java/net/chaosworship/topuslib/DelaunayTest.java
@@ -5,17 +5,61 @@ import net.chaosworship.topuslib.geom2d.Circumcircle;
 import net.chaosworship.topuslib.geom2d.Triangle;
 import net.chaosworship.topuslib.geom2d.Vec2;
 import net.chaosworship.topuslib.geom2d.mesh.DelaunayTriangulator;
+import net.chaosworship.topuslib.geom2d.mesh.Triangulation;
+import net.chaosworship.topuslib.graph.GraphEdgeConsumer;
+import net.chaosworship.topuslib.graph.HashSimpleGraph;
+import net.chaosworship.topuslib.graph.SimpleGraph;
 import net.chaosworship.topuslib.random.SuperRandom;
 
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 import static junit.framework.Assert.*;
 
 
 public class DelaunayTest {
+
+    @Test
+    public void edgeEnumeration() {
+        SuperRandom random = new SuperRandom(1234);
+        DelaunayTriangulator triangulator = new DelaunayTriangulator();
+        ArrayList<Vec2> points = new ArrayList<>();
+        for(int i = 0; i < 30; i++) {
+            points.add(new Vec2(random.nextFloat(), random.nextFloat()));
+        }
+        for(int i = 0; i < 20; i++) {
+            points.add(new Vec2(random.nextFloat(), random.nextFloat()));
+            triangulator.triangulate(points);
+
+            HashSet<IntPair> edgeSetA = new HashSet<>();
+            Triangulation triangulation = triangulator.getTriangulation();
+            for(IntPair ip : triangulation.getEdges()) {
+                edgeSetA.add(IntPair.sorted(ip.a, ip.b));
+            }
+
+            HashSet<IntPair> edgeSetB = new HashSet<>();
+            SimpleGraph graph = new HashSimpleGraph();
+            triangulator.getEdgeGraph(graph);
+            for(IntPair ip : graph.getEdges()) {
+                edgeSetB.add(IntPair.sorted(ip.a, ip.b));
+            }
+
+            final HashSet<IntPair> edgeSetC = new HashSet<>();
+            GraphEdgeConsumer consumer = new GraphEdgeConsumer() {
+                @Override
+                public void putGraphEdge(int a, int b) {
+                    edgeSetC.add(IntPair.sorted(a, b));
+                }
+            };
+            triangulator.putEdges(consumer);
+
+            assertTrue(CollectionTester.intPairSetsEqual(edgeSetA, edgeSetB));
+            assertTrue(CollectionTester.intPairSetsEqual(edgeSetB, edgeSetC));
+        }
+    }
 
     @Test
     public void isDelaunay() {
