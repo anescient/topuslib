8ee284fb6db440171edb915124dc4cfbe52f824e
anescient <anescient@gmail.com>
2018-06-18 13:00:02 -0500

    basic fat-trimming

--- topuslib/src/main/java/net/chaosworship/topuslib/geom3d/Path.java
+++ topuslib/src/main/java/net/chaosworship/topuslib/geom3d/Path.java
@@ -3,7 +3,6 @@ package net.chaosworship.topuslib.geom3d;
 import net.chaosworship.topuslib.geom2d.Arc;
 import net.chaosworship.topuslib.geom2d.Circle;
 import net.chaosworship.topuslib.geom2d.Vec2;
-import net.chaosworship.topuslib.gl.FloatVertexPreBuffer;
 
 import java.util.ArrayList;
 
@@ -23,30 +22,27 @@ public class Path {
     // curve about b, pass through a and c
     public static ArrayList<Vec3> generateCurve(Vec3 a, Vec3 b, Vec3 c, float maxRadius) {
 
-        // translate everything to b = (0,0)
-        a = a.difference(b);
-        c = c.difference(b);
-
         maxRadius = Math.min(maxRadius, Vec3.distance(a, c) / 2);
         if(maxRadius <= 0) {
-            return directPath(a.sum(b), b, c.sum(b));
+            return directPath(a, b, c);
         }
 
-        OrthonormalBasis basis = new OrthonormalBasis().setRightHandedW(a, c);
+        // translate everything to b = (0,0)
+        Vec3 aTrans = a.difference(b);
+        Vec3 cTrans = c.difference(b);
 
-        Vec3 aTrans = basis.transformedToStandardBasis(a);
-        Vec3 cTrans = basis.transformedToStandardBasis(c);
+        OrthonormalBasis basis = new OrthonormalBasis().setRightHandedW(aTrans, cTrans);
 
-        Vec3 startTangent = aTrans.normalized().negate();
-        Vec3 endTangent = cTrans.normalized();
+        Vec3 inTangent = basis.transformedToStandardBasis(aTrans).normalized();
+        Vec3 outTangent = basis.transformedToStandardBasis(cTrans).normalized();
 
-        LazyInternalAngle angle = new LazyInternalAngle(startTangent.negated(), endTangent);
+        LazyInternalAngle angle = new LazyInternalAngle(inTangent, outTangent);
 
         float cosHalfAngle = (float)Math.sqrt((1 + angle.cosine()) / 2);
         float tanHalfAngle = (1 - angle.cosine()) / angle.sine();
 
         if(cosHalfAngle <= 0 || Float.isNaN(cosHalfAngle) || Float.isNaN(tanHalfAngle)) {
-            return directPath(a.sum(b), b, c.sum(b));
+            return directPath(a, b, c);
         }
 
         float radius;
@@ -62,30 +58,31 @@ public class Path {
             trim = radius / tanHalfAngle;
         }
 
-        float h = trim / cosHalfAngle;
-
-        Vec3 inPoint = aTrans.normalized().scale(trim);
-        Vec3 outPoint = cTrans.normalized().scale(trim);
+        Vec3 inPoint = inTangent.scaled(trim);
+        Vec3 outPoint = outTangent.scaled(trim);
         double startAngle = Math.PI + inPoint.getXY().rotated90().negated().atan2();
         double endAngle = Math.PI + outPoint.getXY().rotated90().atan2();
 
         basis.transformFromStandardBasis(inPoint);
         basis.transformFromStandardBasis(outPoint);
+        inPoint.add(b);
+        outPoint.add(b);
 
         Arc arc = new Arc(new Circle(), startAngle, endAngle);
         int n = Math.max((int)(Math.abs(endAngle - startAngle) / 0.1), 3);
 
-        Vec3 circleCenter = startTangent.negated().sum(endTangent).normalize().scale(h);
+        Vec3 circleCenter = inTangent.sum(outTangent).normalize().scale(trim / cosHalfAngle);
 
         ArrayList<Vec3> path = new ArrayList<>();
-        path.add(a.sum(b));
-        path.add(inPoint.sum(b));
+        path.add(a);
+        path.add(inPoint);
         for(Vec2 p2 : arc.getPointsAlongOpen(n)) {
-            Vec3 p = basis.transformedFromStandardBasis(new Vec3(p2.x, p2.y, 0).scaled(radius).sum(circleCenter));
+            Vec3 p = new Vec3(p2.x, p2.y, 0).scale(radius).add(circleCenter);
+            basis.transformFromStandardBasis(p);
             path.add(p.add(b));
         }
-        path.add(outPoint.sum(b));
-        path.add(c.sum(b));
+        path.add(outPoint);
+        path.add(c);
 
         return path;
     }
